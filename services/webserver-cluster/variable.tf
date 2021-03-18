4b328e90cb06625f75e1b2e4ae90dc6afbef27c3
dong huang <huangdgm@gmail.com>
2021-03-18 14:44:11 +0800

    Initial commit of modules repo

---
+++ services/webserver-cluster/variable.tf
@@ -0,0 +1,44 @@
+variable "region" {
+	type = string
+	default = "us-east-2"
+}
+
+variable "cluster-name" {
+	type = string
+	description = "The name to use for all the cluster resources"
+}
+
+variable "db_remote_state_bucket" {
+	type = string
+	description = "The name of the S3 bucket for the db's remote state"
+}
+
+variable "db_remote_state_key" {
+	type = string
+	description = "The path for the db's remote state in S3"
+}
+
+variable "webserver_remote_state_bucket" {
+	type = string
+	description = "The name of the S3 bucket for the webserver's remote state"
+}
+
+variable "webserver_remote_state_key" {
+	type = string
+	description = "The path for the webserver's remote state in S3"
+}
+
+variable "instance_type" {
+	type = string
+	description = "The type of EC2 instances to run (e.g. t2.micro)"
+}
+
+variable "min_size" {
+	type = number
+	description = "The minimum number of EC2 instances in the ASG"
+}
+
+variable "max_size" {
+	type = number
+	description = "The maximum number of EC2 instances in the ASG"
+}
