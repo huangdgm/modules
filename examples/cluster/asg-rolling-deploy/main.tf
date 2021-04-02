5f9f6ffba60681766328bc50ba0c725c83335444
dong huang <huangdgm@gmail.com>
2021-04-02 17:20:03 +0800

    Added the backend file for examples

--- examples/cluster/asg-rolling-deploy/main.tf
+++ examples/cluster/asg-rolling-deploy/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "us-east-2"
 }
 
+terraform {
+  # Only the 'key' parameter remains in the Terraform code, since you still need to set a different 'key' value for each module.
+  # All the other repeated 'backend' arguments, such as 'bucket' and 'region', into a separate file called backend.hcl.
+  backend "s3" {
+    # Terraform will create the key path automatically.
+    # Variables aren't allowed in a backend configuration.
+    key = "modules/examples/cluster/asg-rolling-deploy/terraform.tfstate"
+  }
+}
+
 module "asg" {
   source = "../../../modules/cluster/asg-rolling-deploy"
 
--- examples/databases/mysql/main.tf
+++ examples/databases/mysql/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "us-east-2"
 }
 
+terraform {
+  # Only the 'key' parameter remains in the Terraform code, since you still need to set a different 'key' value for each module.
+  # All the other repeated 'backend' arguments, such as 'bucket' and 'region', into a separate file called backend.hcl.
+  backend "s3" {
+    # Terraform will create the key path automatically.
+    # Variables aren't allowed in a backend configuration.
+    key = "modules/examples/databases/mysql/terraform.tfstate"
+  }
+}
+
 module "mysql_db" {
   source = "../../../modules/databases/mysql"
 
